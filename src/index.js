3d819de10ec42f11fdd5f255693516e160a89c81
ak708 <71438458+ak708@users.noreply.github.com>
2024-05-01 17:58:53 +0530

    1,2,3,4

--- src/index.js
+++ src/index.js
@@ -2,69 +2,16 @@ const parseQuery = require("./queryParser");
 const readCSV = require("./csvReader");
 
 async function executeSELECTQuery(query) {
-  const { fields, table, whereClauses, joinTable, joinCondition } =
-    parseQuery(query);
-  let data = await readCSV(`${table}.csv`);
+  const { fields, table } = parseQuery(query);
+  const data = await readCSV(`${table}.csv`);
 
-  // Perform INNER JOIN if specified
-  if (joinTable && joinCondition) {
-    const joinData = await readCSV(`${joinTable}.csv`);
-    data = data.flatMap((mainRow) => {
-      return joinData
-        .filter((joinRow) => {
-          const mainValue = mainRow[joinCondition.left.split(".")[1]];
-          const joinValue = joinRow[joinCondition.right.split(".")[1]];
-          return mainValue === joinValue;
-        })
-        .map((joinRow) => {
-          return fields.reduce((acc, field) => {
-            const [tableName, fieldName] = field.split(".");
-            acc[field] =
-              tableName === table ? mainRow[fieldName] : joinRow[fieldName];
-            return acc;
-          }, {});
-        });
-    });
-  }
-
-  function evaluateCondition(row, clause) {
-    const { field, operator, value } = clause;
-    switch (operator) {
-      case "=":
-        return row[field] === value;
-      case "!=":
-        return row[field] !== value;
-      case ">":
-        return row[field] > value;
-      case "<":
-        return row[field] < value;
-      case ">=":
-        return row[field] >= value;
-      case "<=":
-        return row[field] <= value;
-      default:
-        throw new Error(`Unsupported operator: ${operator}`);
-    }
-  }
-  // Apply WHERE clause filtering
-  const filteredData =
-    whereClauses.length > 0
-      ? data.filter((row) =>
-          whereClauses.every((clause) => {
-            evaluateCondition(row, clause);
-            // You can expand this to handle different operators
-            return evaluateCondition(row, clause);
-          })
-        )
-      : data;
-
-  // Select the specified fields
-  return filteredData.map((row) => {
-    const selectedRow = {};
+  // Filter the fields based on the query
+  return data.map((row) => {
+    const filteredRow = {};
     fields.forEach((field) => {
-      selectedRow[field] = row[field];
+      filteredRow[field] = row[field];
     });
-    return selectedRow;
+    return filteredRow;
   });
 }
 
